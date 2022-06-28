e99915871d5bb32713c2a42eea21bf1d5930f441
Ricardo Sperlongo DECSKILL <RicardoSperlongo.DECSKILL@edp.com>
2022-06-28 14:49:50 -0300

    update modules and connection to mongodb

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,6 +7,6 @@ import { UserService } from './user.service';
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   providers: [UserService],
   controllers: [],
-  exports: [],
+  exports: [UserService],
 })
 export class UserModule {}
